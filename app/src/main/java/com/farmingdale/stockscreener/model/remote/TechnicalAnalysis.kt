28bedb68d1299fe314b6e09cd601cf83cf16527a
harvey <tsenh@farmingdale.edu>
2024-01-22 22:41:13 -0500

    Add TechnicalAnalysis data class

--- app/src/main/java/com/farmingdale/stockscreener/model/remote/TechnicalAnalysis.kt
+++ app/src/main/java/com/farmingdale/stockscreener/model/remote/TechnicalAnalysis.kt
@@ -4,5 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TechnicalAnalysis(
-    val analysisData: Map<String, AnalysisData>
+    val date: String,
+    val value: String
 )
