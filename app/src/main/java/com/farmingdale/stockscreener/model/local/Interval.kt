6b9c4ac11b6c5104c1dcf4a1233eb28301aca4d4
harvey <tsenh@farmingdale.edu>
2024-01-22 22:37:21 -0500

    Add value to Interval to match expected API calls

--- app/src/main/java/com/farmingdale/stockscreener/model/local/Interval.kt
+++ app/src/main/java/com/farmingdale/stockscreener/model/local/Interval.kt
@@ -1,12 +1,12 @@
 package com.farmingdale.stockscreener.model.local
 
-enum class Interval {
-    ONE_MINUTE,
-    FIVE_MINUTE,
-    FIFTEEN_MINUTE,
-    THIRTY_MINUTE,
-    SIXTY_MINUTE,
-    DAILY,
-    WEEKLY,
-    MONTHLY
+enum class Interval(val value: String) {
+    ONE_MINUTE("1min"),
+    FIVE_MINUTE("5min"),
+    FIFTEEN_MINUTE("15min"),
+    THIRTY_MINUTE("30min"),
+    SIXTY_MINUTE("60min"),
+    DAILY("daily"),
+    WEEKLY("weekly"),
+    MONTHLY("monthly")
 }
