52f1bd99bc1741efd0719966fceeb89720d245a2
harvey <tsenh@farmingdale.edu>
2024-01-24 19:20:19 -0500

    Create data responses for every analysis type

---
+++ app/src/main/java/com/farmingdale/stockscreener/model/remote/BBANDSDataResponse.kt
@@ -0,0 +1,46 @@
+package com.farmingdale.stockscreener.model.remote.responses
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BBANDSDataResponse(
+    @SerialName("Meta Data")
+    val metaData: MetaData,
+
+    @SerialName("Technical Analysis: BBANDS")
+    val technicalAnalysis: Map<String, Analysis>
+) {
+    @Serializable
+    data class MetaData(
+        @SerialName("1: Symbol")
+        val symbol: String,
+
+        @SerialName("2: Indicator")
+        val indicator: String,
+
+        @SerialName("3: Last Refreshed")
+        val lastRefreshed: String,
+
+        @SerialName("6.1: Deviation multiplier for upper band")
+        val deviationMultiplierForUpperBand: Int,
+
+        @SerialName("6.2: Deviation multiplier for lower band")
+        val deviationMultiplierForLowerBand: Int,
+
+        @SerialName("6.3: MA Type")
+        val maType: Int,
+    )
+
+    @Serializable
+    data class Analysis(
+        @SerialName("Real Upper Band")
+        val realUpperBand: String,
+
+        @SerialName("Real Middle Band")
+        val realMiddleBand: String,
+
+        @SerialName("Real Lower Band")
+        val realLowerBand: String
+    )
+}
