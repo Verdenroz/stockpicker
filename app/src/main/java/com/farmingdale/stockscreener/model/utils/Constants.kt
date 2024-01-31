f7c33ab368d5010577de7ee9ecf00c4384e15e56
harvey <tsenh@farmingdale.edu>
2024-01-31 14:35:53 -0500

    Refactor AlphaVantageAPI with rapidapi hosting

--- app/src/main/java/com/farmingdale/stockscreener/model/utils/Constants.kt
+++ app/src/main/java/com/farmingdale/stockscreener/model/utils/Constants.kt
@@ -2,4 +2,4 @@ package com.farmingdale.stockscreener.model.utils
 
 import okhttp3.HttpUrl.Companion.toHttpUrl
 
-val ALPHA_VANTAGE_API_URL = "https://www.alphavantage.co/query?".toHttpUrl()
+val ALPHA_VANTAGE_API_URL = "https://alpha-vantage.p.rapidapi.com".toHttpUrl()
