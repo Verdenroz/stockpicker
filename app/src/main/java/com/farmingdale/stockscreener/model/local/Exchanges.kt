681fccef3e539463f30c440de72e0cef36ea5d72
harvey <tsenh@farmingdale.edu>
2024-02-07 22:08:48 -0500

    Create enums for the available exchanges from FinancialModelPrep

---
+++ app/src/main/java/com/farmingdale/stockscreener/model/local/Exchanges.kt
@@ -0,0 +1,64 @@
+package com.farmingdale.stockscreener.model.local
+
+enum class NorthAmericanExchanges {
+    NYSE,
+    NASDAQ,
+    AMEX,
+    OTC,
+    CBOE,
+    ASE,
+    TSX,
+    TSXV,
+    CNQ,
+    NEO,
+    MEX,
+}
+
+enum class SouthAmericanExchanges {
+    SAO,
+    SGO,
+}
+
+enum class EuropeanExchanges {
+    EURONEXT,
+    XETRA,
+    MCX,
+    LSE,
+    BME,
+    SIX,
+    ICE,
+    MIL,
+    ISE,
+    CPH,
+    HEL,
+    STO,
+    WSE,
+    OSL,
+    JNB,
+    ATH,
+    PRA,
+    BRU
+}
+enum class AsianExchanges {
+    NSE,
+    BSE,
+    JKT,
+    SET,
+    SES,
+    SHZ,
+    SHH,
+    HKSE,
+    TWO,
+    TAI,
+    KLS,
+    JPX,
+    KOE,
+    KSC,
+    TLV,
+    SAU,
+    PNK,
+}
+
+enum class AustralianExchanges {
+    ASX,
+}
