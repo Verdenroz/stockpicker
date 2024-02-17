d21699094f45a319f8d91f540ad7a83690196d3c
harvey <tsenh@farmingdale.edu>
2024-02-16 21:09:29 -0500

    Create enums for the available categories offered by NewsAPI

---
+++ app/src/main/java/com/farmingdale/stockscreener/model/local/news/Category.kt
@@ -0,0 +1,13 @@
+package com.farmingdale.stockscreener.model.local.news
+
+/**
+ * Represents the available categories for News API
+ */
+enum class Category {
+    BUSINESS,
+    ENTERTAINMENT,
+    GENERAL,
+    HEALTH,
+    SCIENCE,
+    SPORTS,
+}
