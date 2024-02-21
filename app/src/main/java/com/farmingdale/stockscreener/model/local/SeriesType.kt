1072b4c2f78532ec0b65f55eed2b322e2ceb43de
harvey <tsenh@farmingdale.edu>
2024-02-21 14:20:48 -0500

    Fix documentation for SeriesType

--- app/src/main/java/com/farmingdale/stockscreener/model/local/SeriesType.kt
+++ app/src/main/java/com/farmingdale/stockscreener/model/local/SeriesType.kt
@@ -2,10 +2,10 @@ package com.farmingdale.stockscreener.model.local
 
 /**
  * The available price types for technical analysis
- * @param OPEN open price
- * @param HIGH high price
- * @param LOW low price
- * @param CLOSE close price
+ * @property OPEN open price
+ * @property HIGH high price
+ * @property LOW low price
+ * @property CLOSE close price
  */
 enum class SeriesType(val value: String) {
     OPEN("open"),
