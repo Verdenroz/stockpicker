fc261d6ac7b55effe19fff8f8176b43349148053
harvey <tsenh@farmingdale.edu>
2024-03-08 11:12:35 -0500

    Create working unit tests for the added google finance functions

--- app/src/test/java/com/farmingdale/stockscreener/GoogleFinanceAPITest.kt
+++ app/src/test/java/com/farmingdale/stockscreener/GoogleFinanceAPITest.kt
@@ -1,5 +1,6 @@
 package com.farmingdale.stockscreener
 
+import com.farmingdale.stockscreener.model.local.UnitedStatesExchanges
 import com.farmingdale.stockscreener.providers.ImplGoogleFinanceAPI
 import com.farmingdale.stockscreener.providers.base.GoogleFinanceAPI
 import kotlinx.coroutines.runBlocking
@@ -23,6 +24,38 @@ class GoogleFinanceAPITest {
     fun getIndicesTest(){
         val info = runBlocking {
             api.getIndices()
+        }
+        println(info)
+    }
+
+    @Test
+    fun getActiveStocksTest(){
+        val info = runBlocking {
+            api.getActiveStocks()
+        }
+        println(info)
+    }
+
+    @Test
+    fun getGainersTest(){
+        val info = runBlocking {
+            api.getGainers()
+        }
+        println(info)
+    }
+
+    @Test
+    fun getLosersTest(){
+        val info = runBlocking {
+            api.getLosers()
+        }
+        println(info)
+    }
+
+    @Test
+    fun getRelatedNewsTest(){
+        val info = runBlocking {
+            api.getRelatedNews("AAPL", UnitedStatesExchanges.NASDAQ)
         }
         println(info)
     }
