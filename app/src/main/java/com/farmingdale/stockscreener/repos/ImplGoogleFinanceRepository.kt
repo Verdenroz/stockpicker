74e1e295749eea2eb4557add32480cf6459d28fb
harvey <tsenh@farmingdale.edu>
2024-03-18 14:49:30 -0400

    Implement repository functions to get active stocks, losing stocks, gaining stocks, and related news of a stock

--- app/src/main/java/com/farmingdale/stockscreener/repos/ImplGoogleFinanceRepository.kt
+++ app/src/main/java/com/farmingdale/stockscreener/repos/ImplGoogleFinanceRepository.kt
@@ -1,5 +1,8 @@
 package com.farmingdale.stockscreener.repos
 
+import com.farmingdale.stockscreener.model.local.UnitedStatesExchanges
+import com.farmingdale.stockscreener.model.local.googlefinance.GoogleFinanceNews
+import com.farmingdale.stockscreener.model.local.googlefinance.GoogleFinanceStock
 import com.farmingdale.stockscreener.model.local.googlefinance.MarketIndex
 import com.farmingdale.stockscreener.providers.ImplGoogleFinanceAPI
 import com.farmingdale.stockscreener.providers.okHttpClient
@@ -9,9 +12,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class ImplGoogleFinanceRepository(): GoogleFinanceRepository() {
+class ImplGoogleFinanceRepository() : GoogleFinanceRepository() {
     private val api = ImplGoogleFinanceAPI(okHttpClient)
-    override suspend fun getIndices(): Flow<List<MarketIndex>> = flow{
+    override suspend fun getIndices(): Flow<List<MarketIndex>> = flow {
         try {
             emit(api.getIndices())
         } catch (e: Exception) {
@@ -19,7 +22,43 @@ class ImplGoogleFinanceRepository(): GoogleFinanceRepository() {
         }
     }.flowOn(Dispatchers.IO)
 
-    companion object{
+    override suspend fun getActives(): Flow<List<GoogleFinanceStock>> = flow {
+        try {
+            emit(api.getActiveStocks())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun getLosers(): Flow<List<GoogleFinanceStock>> = flow {
+        try {
+            emit(api.getLosers())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }.flowOn(Dispatchers.IO)
+
+
+    override suspend fun getGainers(): Flow<List<GoogleFinanceStock>> = flow {
+        try {
+            emit(api.getGainers())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun getNews(
+        symbol: String,
+        exchange: UnitedStatesExchanges
+    ): Flow<List<GoogleFinanceNews>> = flow {
+        try {
+            emit(api.getRelatedNews(symbol, exchange))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }.flowOn(Dispatchers.IO)
+
+    companion object {
         private var repo: ImplGoogleFinanceRepository? = null
 
         /**
