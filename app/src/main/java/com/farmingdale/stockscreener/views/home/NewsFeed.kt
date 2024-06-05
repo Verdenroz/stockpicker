8684d617ed6bb98397fdd74f9c50bd63b66999e7
harvey <harveytseng2@gmail.com>
2024-06-05 10:12:24 -0400

    Remove unused settings icon

--- app/src/main/java/com/farmingdale/stockscreener/views/home/NewsFeed.kt
+++ app/src/main/java/com/farmingdale/stockscreener/views/home/NewsFeed.kt
@@ -16,13 +16,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,7 +28,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,31 +50,19 @@ fun NewsFeed(
     news: List<News>?,
     refresh: () -> Unit,
 ) {
-    var isNewsSettingsOpen by rememberSaveable { mutableStateOf(false) }
-
     Column(
         modifier = Modifier
             .padding(horizontal = 16.dp)
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Row(
+        Text(
+            text = stringResource(id = R.string.news),
+            style = MaterialTheme.typography.titleSmall,
             modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = stringResource(id = R.string.news),
-                style = MaterialTheme.typography.titleSmall
-            )
-            IconButton(onClick = { isNewsSettingsOpen = true }) {
-                Icon(
-                    Icons.Default.Settings,
-                    contentDescription = stringResource(id = R.string.news_settings)
-                )
-            }
-        }
+                .align(Alignment.Start)
+                .padding(vertical = 8.dp)
+        )
         if (news.isNullOrEmpty()) {
             ErrorCard(refresh = refresh)
         } else {
